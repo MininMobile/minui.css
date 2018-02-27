d37108fb6b3323d30fafabe5fc8ddde4be7ddd14
MininMobile <miningamer@gmail.com>
2018-02-27 17:28:14 +0000

    oof wrong tag

--- minui.js
+++ minui.js
@@ -1,14 +1,15 @@
 function init(dark, locs = ["https://raw.githack.com/minin-prod/minui.css/master/minui-common.css",
 							"https://raw.githack.com/minin-prod/minui.css/master/minui-light.css",
 							"https://raw.githack.com/minin-prod/minui.css/master/minui-dark.css"]) {
+	const h = document.getElementsByTagName("head")[0]; // Head
 	const b = document.getElementsByTagName("body")[0]; // Body
 
 	var link = document.createElement("link");
     link.setAttribute("rel", "stylesheet");
     link.setAttribute("type", "text/css");
 	link.setAttribute("href", locs[0]);
-	b.appendChild(link);
+	h.appendChild(link);
 
 	dark ? link.setAttribute("href", locs[2]) : link.setAttribute("href", locs[1]);
-	b.appendChild(link);
+	h.appendChild(link);
 }
