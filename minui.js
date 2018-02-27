b2c3e8730408a68eebf04d82ab3535eee2285607
MininMobile <miningamer@gmail.com>
2018-02-27 16:54:00 +0000

    Append Link

--- minui.js
+++ minui.js
@@ -1,4 +1,12 @@
-function init(dark) {
+function init(dark, local = false, locs = ["common", "light", "dark"]) {
+	// Body
+	const b = document.getElementsByTagName("body")[0];
+
+	var link = new HTMLLinkElement();
+	link.rel = "stylesheet";
+
+	b.appendChild(link);
+
 	if (dark) {
 		
 	} else {
