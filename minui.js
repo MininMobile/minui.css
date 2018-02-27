ddb036beddc2201bc369596790d250aab1c8a303
MininMobile <miningamer@gmail.com>
2018-02-27 16:57:33 +0000

    Insert Proper Locations

--- minui.js
+++ minui.js
@@ -1,9 +1,12 @@
-function init(dark, local = false, locs = ["common", "light", "dark"]) {
+function init(dark, locs = ["https://raw.githubuasercontent.com/minin-prod/minui.css/master/minui-common.css",
+							"https://raw.githubusercontent.com/minin-prod/minui.css/master/minui-light.css",
+							"https://raw.githubusercontent.com/minin-prod/minui.css/master/minui-dark.css"]) {
 	// Body
 	const b = document.getElementsByTagName("body")[0];
 
 	var link = new HTMLLinkElement();
 	link.rel = "stylesheet";
+	link.href = locs[0];
 
 	b.appendChild(link);
 
